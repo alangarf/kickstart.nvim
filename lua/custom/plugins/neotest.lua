95fbfc1b4c1948113136a78068917758498cda98
Alan Garfield <alan@fromorbit.com>
2024-02-13 12:43:12 +1100

    Capturing changes

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -10,9 +10,28 @@ return {
     local neotest = require("neotest")
     neotest.setup {
       adapters = {
-        require("neotest-python") ({
+        require("neotest-python")({
+          dap = { justMyCode = false },
           runner = "pytest",
-        })
+        }),
+      },
+      log_level = 3,
+      consumers = {},
+      icons = {
+        child_indent = "│",
+        child_prefix = "├",
+        collapsed = "─",
+        expanded = "╮",
+        failed = "",
+        final_child_indent = " ",
+        final_child_prefix = "╰",
+        non_collapsible = "─",
+        passed = "",
+        running = "",
+        running_animated = { "/", "|", "\\", "-", "/", "|", "\\", "-" },
+        skipped = "",
+        unknown = "",
+        watching = ""
       }
     }
   end
